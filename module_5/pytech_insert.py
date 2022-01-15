889477c0788a5b2d5014f398d77c69a709401fbb
BUmhunt <74039189+BUmhunt@users.noreply.github.com>
2022-01-15 16:10:32 -0600

    done

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,1 +1,18 @@
+MongoDB: insert_one()
+fred = {
+    "first_name":"Fred"
+}
+
+fred_student_id = students.insert_one(fred).inserted_id
+
+print(fred_student_id)
+
+MongoDB: find()
+docs = db.collection_name.find({})
+print(doc)
+
+MongoDB: find_one() 
+doc = db.collection_name.find_one({"student_id":"1007"})
+
+print(doc["student_id"])
 
