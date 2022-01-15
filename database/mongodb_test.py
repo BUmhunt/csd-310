b26c1b028310be62421e37de7132b441688e2854
Matt <mathunt@my365.bellevue.edu>
2022-01-15 12:45:14 -0600

    new

--- database/mongodb_test.py
+++ database/mongodb_test.py
@@ -2,7 +2,7 @@ import pymongo
 
 from pymongo import MongoClient
 
-url = "mongodb+srv://admin:<password>@cluster0.vayt5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+url = "mongodb+srv://admin:admin@cluster0.vayt5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 client = MongoClient('localhost', 27017)
 
