9711268db1544fe404617c3a4bac0d18a3f8fa3a
BUmhunt <74039189+BUmhunt@users.noreply.github.com>
2022-01-18 18:13:04 -0600

    Add files via upload

---
+++ pytech_update.py
@@ -0,0 +1,49 @@
+import pymongo
+from pymongo import MongoClient
+client = MongoClient("localhost", 27017)
+db = client["pytech"]
+students = db["pytech"]
+
+print("\n -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+result = db.students.update_one({"student_id": 1007}, {set: {"last_name": "Oakenshield II"}})
+
+thorin = {
+    "first_name":"Thorin", 
+    "last_name":"Oakenshield", 
+    "student_id":1007
+}
+print(thorin)
+
+bilbo = {
+    "first_name":"Bilbo", 
+    "last_name":"Baggins", 
+    "student_id":1008
+}
+
+print(bilbo)
+
+frodo = {
+    "first_name":"Frodo", 
+    "last_name":"Baggins", 
+    "student_id":1009
+}
+print(frodo)
+print("\n -- DISPLAYING STUDENTS DOCUMENT FROM 1007 --")
+
+
+thorin2 = {
+    "first_name":"Thorin", 
+    "last_name":"Oakenshield II", 
+    "student_id":1007
+}
+
+doc = db.Students.find_one({"student_id":"1007"})
+print(thorin2)
+
+
+
+
+
+
+
+
