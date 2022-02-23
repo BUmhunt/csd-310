b984b1761efb6e0661e0e41c2c750141e3b7731f
BUmhunt <74039189+BUmhunt@users.noreply.github.com>
2022-02-22 18:14:54 -0600

    Add files via upload

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -1,1 +1,104 @@
+DROP USER IF EXISTS 'whatabook_user'@'localhost';
 
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+
+ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
+ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
+
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE store (
+    store_id    INT             NOT NULL    AUTO_INCREMENT,
+    locale      VARCHAR(500)    NOT NULL,
+    PRIMARY KEY(store_id)
+);
+
+CREATE TABLE book (
+    book_id     INT             NOT NULL    AUTO_INCREMENT,
+    book_name   VARCHAR(200)    NOT NULL,
+    author      VARCHAR(200)    NOT NULL,
+    details     VARCHAR(500),
+    PRIMARY KEY(book_id)
+);
+
+CREATE TABLE user (
+    user_id         INT         NOT NULL    AUTO_INCREMENT,
+    first_name      VARCHAR(75) NOT NULL,
+    last_name       VARCHAR(75) NOT NULL,
+    PRIMARY KEY(user_id) 
+);
+
+CREATE TABLE wishlist (
+    wishlist_id     INT         NOT NULL    AUTO_INCREMENT,
+    user_id         INT         NOT NULL,
+    book_id         INT         NOT NULL,
+    PRIMARY KEY (wishlist_id),
+    CONSTRAINT fk_book
+    FOREIGN KEY (book_id)
+        REFERENCES book(book_id),
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+        REFERENCES user(user_Id)
+);
+
+INSERT INTO store(locale)
+    VALUES('1000 Galvin Rd S, Bellevue, NE 68005');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Return of the King', 'J.R.Tolkien', 'The third part of The Lord of the Rings');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Fellowship of the Ring', 'J.R.Tolkien', 'The first part of The Lord of the Rings');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Two Towers', 'J.R.Tolkien', "The second part of The Lord of The Rings");
+
+INSERT INTO book(book_name, author)
+    VALUES('The Catcher in the Rye', 'J.D. Salinger');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Eagle Has Landed', 'Jack Higgins');
+
+INSERT INTO book(book_name, author)
+    VALUES('Watership Down', 'Richard Adams');
+
+INSERT INTO book(book_name, author)
+    VALUES('Charlottes Web', 'E.B. White');
+
+INSERT INTO book(book_name, author)
+    VALUES('To Kill a Mockingbird', 'Harper Lee');
+
+INSERT INTO book(book_name, author)
+    VALUES('War and Peace', 'Leo Tolstoy');
+
+INSERT INTO user(first_name, last_name) 
+    VALUES('Bill', 'Smith');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Joe', 'Doe');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Matt', 'Hunt');
+
+INSERT INTO wishlist(user_id, book_id) 
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Bill'), 
+        (SELECT book_id FROM book WHERE book_name = 'Charlottes Web')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Joe'),
+        (SELECT book_id FROM book WHERE book_name = 'War and Peace')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Matt'),
+        (SELECT book_id FROM book WHERE book_name = 'To Kill a Mockingbird')
+    );
